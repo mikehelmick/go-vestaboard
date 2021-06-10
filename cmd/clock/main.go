11bf6b07f331232427f3416f4dee3be1c51d1fc8
Mike Helmick <mike.helmick@gmail.com>
2021-06-09 22:44:24 -0700

    clock demo
    Signed-off-by: Mike Helmick <mike.helmick@gmail.com>

---
+++ cmd/clock/main.go
@@ -0,0 +1,54 @@
+// Copyright 2021 Mike Helmick
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"github.com/mikehelmick/go-vestaboard"
+	"github.com/mikehelmick/go-vestaboard/internal/config"
+)
+
+func main() {
+	flag.Parse()
+
+	ctx := context.Background()
+	c, err := config.New(ctx)
+	if err != nil {
+		log.Fatalf("error loading config: %v", err)
+	}
+
+	client := vestaboard.New(c.APIKey, c.Secret)
+
+	subs, err := client.Subscriptions(ctx)
+	if err != nil {
+		log.Fatalf("error calling Viewer: %v", err)
+	}
+	log.Printf("result: %+v", subs)
+
+	for {
+		t := time.Now()
+		display := t.Format(time.RFC1123)
+		_, err := client.SendText(ctx, subs.Subscriptions[0].ID, display)
+		if err != nil {
+			log.Fatalf("error sending message: %v", err)
+		}
+		time.Sleep(15 * time.Second)
+	}
+
+}
